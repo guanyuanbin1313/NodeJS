6084a82d3da36eaf8593b1846ff492d6c7f5d357
guanyuanbin1313 <2925220648@qq.com>
2019-10-20 20:29:51 +0800

    workthree

---
+++ work/workthree/blogSystem.js
@@ -0,0 +1,229 @@
+const http = require('http');
+const url = require('url');
+const path = require('path');
+const fs = require('fs');
+const querystring = require('querystring');
+
+let chapterList = [{
+        "chapterId": 1,
+        "chapterName": "hello ,这是后台取到的数据",
+        "imgPath": "images/1442457564979540.jpeg",
+        "chapterDes": "注定，有些路，只能一个人走；有些事，",
+        "chapterContent": "有人说，生活生活，就是生下来活下去，也有人说，生容易，活容易，生活不容易。有个朋友曾经和我说，这个世界，大多数人都没有生活，他们有的仅仅是生存。我赞同这种说法吧，只是生存，对于我们可能都是一种奢侈。每个人都在努力，努力为着这个本来不属于我们的生活和努力。对于含着金钥匙而成长的人，我们又羡慕又嫉妒。可我们没有想过，他们的先人在斩荆棘的时候，付出的艰辛和艰苦，才有了后代荣华。生活是什么,其实在我看来，生活是一种需要，在你最需要亲人的时候，亲人已经突然不在了。子欲养而亲不待，说得成其深，对于我们是一种思考。他们的离开，对于我们都是一种打击，可也是一种思考。离开时痛苦的，可是生活就是有生老病死，当你一直都习惯存在的人已经离开的时候，慢慢地，你会看透很多，这就是你面对生活所需要的。生活是什么？其实在我看来，生活就是一种打拼，当过年过节的时候你看着别人的父母带着自己的孩子快乐的回家的时候、尔你还只能在大街上流浪的时候。许多游子和打工的人或许都有这种感觉吧，为了赚钱，为了养家。当我们看到留守儿童那迷茫孤寂的眼神，不由得心痛的时候，或许你能理解那份父母为了儿女在外打拼的奔波吧。其实在我看来，生活就是一种漠然，当你真正遇到困难而朋友都不能帮你的时候、怕你还不了钱的时候。你只能自己默默面对这一切，这一切让你十分无奈，可是你明白了一个道理，借给你是道理，不借给你是合理。人之常情，所以，想怪不能怪的时候，你留下的只是无奈。</p><p>生活是什么？</p><p>其实生活就是无奈的活着，不得不无奈的活着。我们活在这个世界上，为的只是寻找一个目标，而生活，给我们的感受，却是无奈与绝望的迷茫。这时候你不能说生活是美好和美满的吧？可是正因为有痛苦，无奈与悲伤，我们才有开心的日子。也正因为有了生存，才有了生活。</p><p>无奈的生活，才给我们无奈的心情，无奈的心情才有了悠然见南山的豁达与开朗，这就是生活，这种生活才会有着无限的魅力，也只有这种魅力才能吸引我们一直向前",
+        "publishTimer": "2019-08-19",
+        "author": "admin",
+        "views": 1022
+    },
+    {
+        "chapterId": 2,
+        "chapterName": "那些不是事",
+        "imgPath": "images/1442201163344838-lp.jpg",
+        "chapterDes": "人生，原本就该这样。再大的事，无非是个经历而己。明天的太阳照样升起，就像罗俊杰个人博客​的时候水来土掩，兵来将挡。任何事情都会成为过去，只要有好的心态，就可以面对人生的.",
+        "chapterContent": "有人说，生活生活，就是生下来活下去，也有人说，生容易，活容易，生活不容易。有个朋友曾经和我说，这个世界，大多数人都没有生活，他们有的仅仅是生存。我赞同这种说法吧，只是生存，对于我们可能都是一种奢侈。每个人都在努力，努力为着这个本来不属于我们的生活和努力。对于含着金钥匙而成长的人，我们又羡慕又嫉妒。可我们没有想过，他们的先人在斩荆棘的时候，付出的艰辛和艰苦，才有了后代荣华。生活是什么,其实在我看来，生活是一种需要，在你最需要亲人的时候，亲人已经突然不在了。子欲养而亲不待，说得成其深，对于我们是一种思考。他们的离开，对于我们都是一种打击，可也是一种思考。离开时痛苦的，可是生活就是有生老病死，当你一直都习惯存在的人已经离开的时候，慢慢地，你会看透很多，这就是你面对生活所需要的。生活是什么？其实在我看来，生活就是一种打拼，当过年过节的时候你看着别人的父母带着自己的孩子快乐的回家的时候、尔你还只能在大街上流浪的时候。许多游子和打工的人或许都有这种感觉吧，为了赚钱，为了养家。当我们看到留守儿童那迷茫孤寂的眼神，不由得心痛的时候，或许你能理解那份父母为了儿女在外打拼的奔波吧。其实在我看来，生活就是一种漠然，当你真正遇到困难而朋友都不能帮你的时候、怕你还不了钱的时候。你只能自己默默面对这一切，这一切让你十分无奈，可是你明白了一个道理，借给你是道理，不借给你是合理。人之常情，所以，想怪不能怪的时候，你留下的只是无奈。</p><p>生活是什么？</p><p>其实生活就是无奈的活着，不得不无奈的活着。我们活在这个世界上，为的只是寻找一个目标，而生活，给我们的感受，却是无奈与绝望的迷茫。这时候你不能说生活是美好和美满的吧？可是正因为有痛苦，无奈与悲伤，我们才有开心的日子。也正因为有了生存，才有了生活。</p><p>无奈的生活，才给我们无奈的心情，无奈的心情才有了悠然见南山的豁达与开朗，这就是生活，这种生活才会有着无限的魅力，也只有这种魅力才能吸引我们一直向前",
+        "publishTimer": "2019-08-19",
+        "author": "admin",
+        "views": 102
+    },
+    {
+        "chapterId": 3,
+        "chapterName": "只想，留住心中的那份静美",
+        "imgPath": "images/1442642178239101-lp.jpg",
+        "chapterDes": "在这个鸟语花香的季节,踏着早晨罗俊杰个人博客的一缕光芒在小路间走着,呼吸着这自然的清晰空气，只想，衣袂飘飘，信步于绿荫小道，或俯首听花开，或低眉赏花香，盈脉脉春意，于眼波中...",
+        "chapterContent": "有人说，生活生活，就是生下来活下去，也有人说，生容易，活容易，生活不容易。有个朋友曾经和我说，这个世界，大多数人都没有生活，他们有的仅仅是生存。我赞同这种说法吧，只是生存，对于我们可能都是一种奢侈。每个人都在努力，努力为着这个本来不属于我们的生活和努力。对于含着金钥匙而成长的人，我们又羡慕又嫉妒。可我们没有想过，他们的先人在斩荆棘的时候，付出的艰辛和艰苦，才有了后代荣华。生活是什么,其实在我看来，生活是一种需要，在你最需要亲人的时候，亲人已经突然不在了。子欲养而亲不待，说得成其深，对于我们是一种思考。他们的离开，对于我们都是一种打击，可也是一种思考。离开时痛苦的，可是生活就是有生老病死，当你一直都习惯存在的人已经离开的时候，慢慢地，你会看透很多，这就是你面对生活所需要的。生活是什么？其实在我看来，生活就是一种打拼，当过年过节的时候你看着别人的父母带着自己的孩子快乐的回家的时候、尔你还只能在大街上流浪的时候。许多游子和打工的人或许都有这种感觉吧，为了赚钱，为了养家。当我们看到留守儿童那迷茫孤寂的眼神，不由得心痛的时候，或许你能理解那份父母为了儿女在外打拼的奔波吧。其实在我看来，生活就是一种漠然，当你真正遇到困难而朋友都不能帮你的时候、怕你还不了钱的时候。你只能自己默默面对这一切，这一切让你十分无奈，可是你明白了一个道理，借给你是道理，不借给你是合理。人之常情，所以，想怪不能怪的时候，你留下的只是无奈。</p><p>生活是什么？</p><p>其实生活就是无奈的活着，不得不无奈的活着。我们活在这个世界上，为的只是寻找一个目标，而生活，给我们的感受，却是无奈与绝望的迷茫。这时候你不能说生活是美好和美满的吧？可是正因为有痛苦，无奈与悲伤，我们才有开心的日子。也正因为有了生存，才有了生活。</p><p>无奈的生活，才给我们无奈的心情，无奈的心情才有了悠然见南山的豁达与开朗，这就是生活，这种生活才会有着无限的魅力，也只有这种魅力才能吸引我们一直向前",
+        "publishTimer": "2019-08-19",
+        "author": "admin",
+        "views": 202
+    },
+    {
+        "chapterId": 4,
+        "chapterName": "所以才会美",
+        "imgPath": "images/1442539025939884-lp.jpg",
+        "chapterDes": "人，越简单就会越快乐;水，掺杂东西越少才会显得清澈靓丽。大自然越安静,才会体现它的本质。人生就是这样，倘若有运，不用祈求，因为祈求也是无用。倘若无运，无需悲伤，因为悲...",
+        "chapterContent": "有人说，生活生活，就是生下来活下去，也有人说，生容易，活容易，生活不容易。有个朋友曾经和我说，这个世界，大多数人都没有生活，他们有的仅仅是生存。我赞同这种说法吧，只是生存，对于我们可能都是一种奢侈。每个人都在努力，努力为着这个本来不属于我们的生活和努力。对于含着金钥匙而成长的人，我们又羡慕又嫉妒。可我们没有想过，他们的先人在斩荆棘的时候，付出的艰辛和艰苦，才有了后代荣华。生活是什么,其实在我看来，生活是一种需要，在你最需要亲人的时候，亲人已经突然不在了。子欲养而亲不待，说得成其深，对于我们是一种思考。他们的离开，对于我们都是一种打击，可也是一种思考。离开时痛苦的，可是生活就是有生老病死，当你一直都习惯存在的人已经离开的时候，慢慢地，你会看透很多，这就是你面对生活所需要的。生活是什么？其实在我看来，生活就是一种打拼，当过年过节的时候你看着别人的父母带着自己的孩子快乐的回家的时候、尔你还只能在大街上流浪的时候。许多游子和打工的人或许都有这种感觉吧，为了赚钱，为了养家。当我们看到留守儿童那迷茫孤寂的眼神，不由得心痛的时候，或许你能理解那份父母为了儿女在外打拼的奔波吧。其实在我看来，生活就是一种漠然，当你真正遇到困难而朋友都不能帮你的时候、怕你还不了钱的时候。你只能自己默默面对这一切，这一切让你十分无奈，可是你明白了一个道理，借给你是道理，不借给你是合理。人之常情，所以，想怪不能怪的时候，你留下的只是无奈。</p><p>生活是什么？</p><p>其实生活就是无奈的活着，不得不无奈的活着。我们活在这个世界上，为的只是寻找一个目标，而生活，给我们的感受，却是无奈与绝望的迷茫。这时候你不能说生活是美好和美满的吧？可是正因为有痛苦，无奈与悲伤，我们才有开心的日子。也正因为有了生存，才有了生活。</p><p>无奈的生活，才给我们无奈的心情，无奈的心情才有了悠然见南山的豁达与开朗，这就是生活，这种生活才会有着无限的魅力，也只有这种魅力才能吸引我们一直向前",
+        "publishTimer": "2019-08-19",
+        "author": "admin",
+        "views": 102
+    }
+];
+
+//用户信息
+let userList = [
+    {
+        username: "admin",
+        pwd: "admin"
+    }, {
+        username: "gyb",
+        pwd: "123"
+    },
+    {
+        username: "123",
+        pwd: "123"
+    }
+];
+
+
+
+
+http.createServer(function (req, res) {
+    var urlObj = url.parse(req.url);
+    var pathName = urlObj.pathname;
+
+    let img = /(^\/images).*/gi;
+    let css = /(^\/css).*/gi;
+
+    if (img.test(pathName)) {
+        showImage(pathName, res);
+    } else if (css.test(pathName)) {
+        showCss(pathName, res);
+    } else if (pathName == "/js/baiduTemplate.js") {
+        showJs(res);
+    } else if (pathName == "/list") {
+        showList(res);
+    } else if (pathName == "/login") {
+        showLogin(res);
+    } else if (pathName == "/listmanager") {
+        showListm(res);
+    } else if (pathName == "/addChapter") {
+        showAdd(res);
+    } else if (pathName == "/login/gyb") {
+        getPwd(req, res);
+    } else if (pathName == "/detail") {
+        showDetail(res);
+    } else if (pathName == "/getchapterlist") {
+        showGetch(res);
+    }
+
+
+}).listen(8083);
+console.log("server is listening 8083");
+
+function showImage(img, res) {
+    let imgPath = path.join(__dirname, img);
+    let imgContent = fs.readFileSync(imgPath);
+    res.writeHead(200, {
+        "Content-Type": "image/jpg"
+    });
+    res.write(imgContent);
+    res.end();
+}
+
+
+function showCss(pn, res) {
+    let cssPath = path.join(__dirname, pn);
+    let cssContent = fs.readFileSync(cssPath);
+    res.writeHead(200, {
+        "Content-Type": "text/css"
+    });
+    res.write(cssContent);
+    res.end();
+}
+
+function showJs(res) {
+    let jsPath = path.join(__dirname, "./js/baiduTemplate.js");
+    let jsContent = fs.readFileSync(jsPath);
+    res.writeHead(200, {
+        "Content-Type": "text/javascript"
+    });
+    res.write(jsContent);
+    res.end();
+}
+
+//列表页:chapterList.html
+function showList(res) {
+    var listPath = path.join(__dirname, "/chapterList.html")
+    var fileContent = fs.readFileSync(listPath);
+    res.writeHead(200, {
+        "content-type": "text/html"
+    });
+    res.write(fileContent);
+    res.end();
+
+}
+
+//后台登录页面:login.html
+function showLogin(res) {
+    var loginPath = path.join(__dirname, "/login.html")
+    var fileContent = fs.readFileSync(loginPath);
+    res.writeHead(200, {
+        "Content-type": "text/html"
+    });
+    res.write(fileContent);
+    res.end();
+}
+
+//后台文章列表页面:list.html
+function showListm(res) {
+    var listmPath = path.join(__dirname, "/list.html")
+    var fileContent = fs.readFileSync(listmPath);
+    res.writeHead(200, {
+        "content-type": "text/html"
+    })
+    res.write(fileContent);
+    res.end();
+}
+
+//后台添加文章页面:addChapter.html
+function showAdd(res) {
+    var listPath = path.join(__dirname, "/addChapter.html")
+    var fileContent = fs.readFileSync(listPath);
+    res.writeHead(200, {
+        "content-type": "text/html"
+    })
+    res.write(fileContent);
+    res.end();
+}
+
+
+
+//全文
+function showDetail(res) {
+    let indexPath = path.join(__dirname, "./chapter.html");
+    let fileContent = fs.readFileSync(indexPath);
+    res.writeHead(200, {
+        "Content-type": "text/html"
+    });
+    res.write(fileContent);
+    res.end();
+}
+
+function showGetch(res) {
+    let dataStr = JSON.stringify(chapterList);
+    res.writeHead(200, {
+        "Content-Type": "application/json"
+    });
+    res.write(dataStr);
+    res.end();
+}
+
+
+function getPwd(req, res) {
+    var postData = "";
+    req.on("data", function (chunk) {
+        postData += chunk;
+    })
+    req.on("end", function () {
+        postData = querystring.parse(postData);
+        let username = postData.username;
+        let pwd = postData.password;
+        for (var i = 0; i < userList.length; i++) {
+            if (username == userList[i].username && pwd == userList[i].pwd) {
+                let str = "登陆成功";
+                str = JSON.stringify(str);
+                res.writeHead(200, {
+                    "Content-Type": "text/plain"
+                });
+                res.write(str);
+                res.end();
+                return;
+            }
+        }
+        let err = "用户名或密码错误！";
+        err = JSON.stringify(err);
+        res.writeHead(200, {
+            "Content-Type": "text/plain"
+        });
+        res.write(err);
+        res.end();
+
+    })
+}
